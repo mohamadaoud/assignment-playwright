2050850103686824a77848087b946935e04f188d
Mohamad Daoud <daoud_mohamad@yahoo.co.uk>
2025-12-06 22:53:35 +0100

    Delete api folder and its files

--- tests/api/get_student.test.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@playwright/test"
-
-test("Get student", async ({request}) => {
-
-    const headers =  {API_KEY: "HELLOME"}
-
-    const response = await request.get("https://test-379574553568.us-central1.run.app/student", {headers: headers})
-    console.log(await response.json())
-})
